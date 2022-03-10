f00923d9a7f468ff65a4901c2fc92ca8404db57c
PtDr <onjamamonjy@gmail.com>
2022-03-10 20:00:00 +0300

    first init commit

---
+++ src/main/java/com/example/securityCannevas/SecurityCannevasApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityCannevas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityCannevasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityCannevasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/securityCannevas/SecurityCannevasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityCannevas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityCannevasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
